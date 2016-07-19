0c04b4ba7976948c2b2b6b9b5345bcb05d19f1f8
Yaqi Yang <yaya@microsoft.com>
2016-07-19 13:56:00 -0700

    Update module descriptions

--- PhpGenTest.bat
+++ PhpGenTest.bat
@@ -1,6 +1,6 @@
 REM -- Must change the following two lines to point to the correct local folders!
 set autorest=src\core\AutoRest\bin\Debug\net451\win7-x64\AutoRest.exe
-set outfolder=D:\Repos\php-sdk-dev\src\Arm
+set outfolder=D:\Repos\azure-sdk-for-php-arm\src\Arm
 
 set master=https://raw.githubusercontent.com/azure/azure-rest-api-specs/master
 
